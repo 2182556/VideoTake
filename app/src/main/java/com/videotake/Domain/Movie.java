1f77d4894efdd6285bd873ecc61faa092973a104
Bramgelten <bramgelten@gmail.com>
2022-03-30 17:08:27 +0200

    Created Movie domain

--- app/src/main/java/com/videotake/Domain/Movie.java
+++ app/src/main/java/com/videotake/Domain/Movie.java
@@ -1,7 +1,26 @@
 package com.videotake.Domain;
 
+import java.util.Date;
+
 public class Movie {
-    //joejoe
-    //joejoe2
-    //joejoe3 final version
+
+    private String MovieID;
+    private String MovieName;
+    private String Description;
+    private String GenreID;
+    private Double Rating;
+    private Date ReleaseDate;
+
+    public String getMovieName() {
+        return MovieName;
+    }
+
+    public String getMovieDescription() {
+        return Description;
+    }
+
+    public Double getRating() {
+        return Rating;
+    }
+
 }
