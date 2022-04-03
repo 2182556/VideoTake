f226b3369d854fdb9f2283b96480b69587c8d2ab
2182556 <je.boellaard@student.avans.nl>
2022-04-03 17:52:59 +0200

    added popup menu to listitem in listoverview

--- app/src/main/java/com/videotake/UI/Adapters/MovieListOverviewAdapter.java
+++ app/src/main/java/com/videotake/UI/Adapters/MovieListOverviewAdapter.java
@@ -2,10 +2,14 @@ package com.videotake.UI.Adapters;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -24,20 +28,66 @@ public class MovieListOverviewAdapter extends RecyclerView.Adapter<MovieListOver
     private final LayoutInflater mInflater;
 
     public MovieListOverviewAdapter(Context context) {
-            mInflater = LayoutInflater.from(context);
-            }
+        mInflater = LayoutInflater.from(context);
+    }
 
     @NonNull
     @Override
     public MovieListOverviewAdapter.MovieListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View mItemView = mInflater.inflate(R.layout.listoverview_item,parent,false);
-            return new MovieListViewHolder(mItemView,this);
-            }
+        View mItemView = mInflater.inflate(R.layout.listoverview_item,parent,false);
+        return new MovieListViewHolder(mItemView,this);
+    }
 
     @Override
     public void onBindViewHolder(@NonNull MovieListOverviewAdapter.MovieListViewHolder holder, int position) {
         MovieList mCurrent = allLists.get(position);
         holder.titleList.setText(mCurrent.getListName());
+        holder.menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showPopupMenu(holder.menu, holder.getAdapterPosition());
+            }
+        });
+    }
+
+    public void showPopupMenu(View view, int position){
+        PopupMenu popup = new PopupMenu(mInflater.getContext(), view, Gravity.END);
+        MenuInflater inflater = popup.getMenuInflater();
+
+        inflater.inflate(R.menu.menu_popup_list, popup.getMenu());
+
+        //set menu item click listener here
+        popup.setOnMenuItemClickListener(new PopupMenuListOnClickListener(position));
+        popup.show();
+    }
+
+    class PopupMenuListOnClickListener implements PopupMenu.OnMenuItemClickListener {
+        int position;
+
+        PopupMenuListOnClickListener(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.delete_list:
+                    // ...
+                    return true;
+//                case R.id.edit:
+//                    // ...
+//                    return true;
+//                case R.id.delete:
+//                    // ...
+//                    return true;
+//                case R.id.favourite:
+//                    // ...
+//                    return true;
+//                default:
+            }
+            return false;
+        }
     }
 
     public void setData(List<MovieList> items) {
@@ -58,10 +108,12 @@ public class MovieListOverviewAdapter extends RecyclerView.Adapter<MovieListOver
     class MovieListViewHolder extends RecyclerView.ViewHolder {
         final MovieListOverviewAdapter adapter;
         public final TextView titleList;
+        public final ImageView menu;
 
         public MovieListViewHolder(@NonNull View itemView, MovieListOverviewAdapter adapter) {
             super(itemView);
             titleList = itemView.findViewById(R.id.rec_list_title);
+            menu = itemView.findViewById(R.id.list_settings);
             this.adapter = adapter;
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
