3786ad31d34d8c5c3114faeef3b8586bc1482d4f
2182556 <je.boellaard@student.avans.nl>
2022-04-03 17:38:39 +0200

    changed layout listoverview_item.xml

--- app/src/main/java/com/videotake/UI/Lists/ListOverviewFragment.java
+++ app/src/main/java/com/videotake/UI/Lists/ListOverviewFragment.java
@@ -23,6 +23,7 @@ import com.videotake.Logic.User.LoginViewModel;
 import com.videotake.Logic.User.LoginViewModelFactory;
 import com.videotake.UI.Adapters.MovieListOverviewAdapter;
 import com.videotake.databinding.FragmentHomeBinding;
+import com.videotake.databinding.FragmentListOverviewBinding;
 
 import java.util.List;
 
@@ -31,16 +32,16 @@ public class ListOverviewFragment extends Fragment {
     private MovieViewModel movieViewModel;
     private LoginViewModel loginViewModel;
 
-    private FragmentHomeBinding binding;
+    private FragmentListOverviewBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        binding = FragmentListOverviewBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
         MovieListOverviewAdapter mAdapter = new MovieListOverviewAdapter(inflater.getContext());
-        RecyclerView mRecyclerView = binding.recyclerview;
+        RecyclerView mRecyclerView = binding.recyclerviewList;
         mRecyclerView.setAdapter(mAdapter);
 
         loginViewModel = new ViewModelProvider(this, new LoginViewModelFactory())
@@ -60,6 +61,8 @@ public class ListOverviewFragment extends Fragment {
                 }
             }
         });
+
+        //section to place get button and make onclick addlist (with popup?)
         return root;
     }
 
