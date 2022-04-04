7abe5fe1bd92f20450948ce3563d4f5c01d4eced
Bananakingo <vandenbosch2003@gmail.com>
2022-04-04 11:57:17 +0200

    Created MovieDao and updated MovieRoomDatabase.java

---
+++ app/src/main/java/com/videotake/DAL/MovieDao.java
@@ -0,0 +1,22 @@
+package com.videotake.DAL;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.videotake.Domain.Movie;
+
+import java.util.List;
+
+@Dao
+public interface MovieDao {
+    @Insert
+    void insert(Movie movie);
+
+    @Query("DELETE FROM movie_table")
+    void deleteAll();
+
+    @Query("SELECT * from movie_table ORDER BY movie ASC")
+    LiveData<List<Movie>> getAllMovies();
+}
--- app/src/main/java/com/videotake/DAL/MovieRoomDatabase.java
+++ app/src/main/java/com/videotake/DAL/MovieRoomDatabase.java
@@ -1,7 +1,27 @@
 package com.videotake.DAL;
 
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.videotake.Domain.Movie;
+
+@Database(entities = {Movie.class}, version = 1, exportSchema = false)
 public abstract class MovieRoomDatabase extends RoomDatabase {
 
+    public abstract MovieDao movieDao();
+    private static MovieRoomDatabase INSTANCE;
+
+    static MovieRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (MovieRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), MovieRoomDatabase.class, "movie_database").fallbackToDestructiveMigration().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 }
