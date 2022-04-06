385b6ea17ba6e6bfaf7d28e6577727027fe68fae
2182556 <je.boellaard@student.avans.nl>
2022-04-06 15:25:03 +0200

    added tabs for discover and search

--- app/src/main/java/com/videotake/UI/Home/HomeFragment.java
+++ app/src/main/java/com/videotake/UI/Home/HomeFragment.java
@@ -6,80 +6,60 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTabHost;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.videotake.Domain.MovieList;
 import com.videotake.Logic.User.LoggedInUserViewModel;
 import com.videotake.Logic.User.LoginViewModel;
+import com.videotake.R;
+import com.videotake.UI.Adapters.HomeFragmentsAdapter;
 import com.videotake.UI.Adapters.MovieListAdapter;
 import com.videotake.UI.DetailPage.MovieDetailsViewModel;
 import com.videotake.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
     private final String TAG_NAME = HomeFragment.class.getSimpleName();
-    private HomeViewModel homeViewModel;
-    private LoginViewModel loginViewModel;
-    private MovieDetailsViewModel movieDetailsViewModel;
-    private MovieListAdapter adapter;
+    HomeFragmentsAdapter collectionAdapter;
+    ViewPager2 viewPager;
 
-    private FragmentHomeBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
-        movieDetailsViewModel = new ViewModelProvider(this).get(MovieDetailsViewModel.class);
-        if (loginViewModel.getLoggedInUser()!=null){
-            LoggedInUserViewModel loggedInUserViewModel = new ViewModelProvider(this).get(LoggedInUserViewModel.class);
-            adapter = new MovieListAdapter(
-                    inflater.getContext(), loggedInUserViewModel,movieDetailsViewModel);
-        } else {
-            adapter = new MovieListAdapter(inflater.getContext(),movieDetailsViewModel);
-        }
-
-
-        RecyclerView mRecyclerView = binding.recyclerview;
-        mRecyclerView.setAdapter(adapter);
-
-        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-        MovieList trendingList = homeViewModel.getTrendingMovieList();
-        if (trendingList!=null) {
-            adapter.setData(trendingList.getMovies());
-        } else {
-            homeViewModel.getTrendingMovies();
-            homeViewModel.getTrendingListResult().observe(getViewLifecycleOwner(), result -> {
-                if (result == null) { return; }
-                if (result.getError() == null) {
-                    adapter.setData(homeViewModel.getTrendingMovieList().getMovies());
-                } else {
-                    Log.d(TAG_NAME, "An error occurred when trying to load trending movies");
-                }
-            });
-        }
-
-        ImageView filter_button = binding.btnExpandFilterOptions;
-        filter_button.setOnClickListener(v -> {
-            if (binding.hiddenView.getVisibility() == View.GONE){
-                binding.hiddenView.setVisibility(View.VISIBLE);
-            } else {
-                binding.hiddenView.setVisibility(View.GONE);
-            }
-
-        });
-        return root;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        collectionAdapter = new HomeFragmentsAdapter(this);
+        viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(collectionAdapter);
+        collectionAdapter.addFragment(new DiscoverFragment());
+        collectionAdapter.addFragment(new SearchFragment());
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            if(position == 0)
+                tab.setText("DISCOVER");
+            else
+                tab.setText("SEARCH");
+        }).attach();
     }
 }
+
